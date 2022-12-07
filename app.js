1a76b726ce7c10e5ca74339fbb9cd746f31159be
Elaminebendaas <33202205+Elaminebendaas@users.noreply.github.com>
2022-12-07 16:25:40 -0500

    finally adding rows with attributes!

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
+//New row creation
+const addRowBtn = document.getElementById('addRow')
+const table = document.getElementById("table")
 
+addRowBtn.addEventListener('click', newRow)
+
+function newRow(){
+    let newRow = document.createElement('tr')
+    const nameInput = document.createElement('input')
+    const gradeInput = document.createElement('input')
+    const weightInput = document.createElement('input')
+
+
+    nameInput.setAttribute('type', 'text')
+    nameInput.setAttribute('placeholder','e.g Midterm 1')
+
+    gradeInput.setAttribute('type', 'number')
+
+    weightInput.setAttribute('type','number')
+
+
+    table.insertRow()
+    newRow.append(nameInput)
+    newRow.append(gradeInput)
+    newRow.append(weightInput)
+
+}
